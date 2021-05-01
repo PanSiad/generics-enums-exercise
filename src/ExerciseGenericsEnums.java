ebe4bf55ec91a46ae4eadb92c48c9e1d36abf5e7
PanSiad <panosanas@hotmail.com>
2021-05-01 10:29:26 +0300

    initial commit

--- src/ExerciseGenericsEnums.java
+++ src/ExerciseGenericsEnums.java
@@ -21,37 +21,81 @@ public class ExerciseGenericsEnums {
 
     private enum Priority {
         HIGH, MEDIUM, LOW
+
+        //Exercise 3
+        //enum constants cannot be used as generic, single type values are assigned
     }
 
-//    private static class FavoriteClasses {
-//        private favorite1;
-//        private favorite2;
-//        private favorite3;
-//
-//        FavoriteClasses(fav1, fav2, fav3) {
-//            this.favorite1 = fav1;
-//            this.favorite2 = fav2;
-//            this.favorite3 = fav3;
-//        }
-//
-//        public getFav1() {
-//            return (this.favorite1);
-//        }
-//
-//        public getFav2() {
-//            return (this.favorite2);
-//        }
-//
-//        public getFav3() {
-//            return (this.favorite3);
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        List r = new ArrayList();
-//        r.add(6.3);
-//        r.add(5.9);
-//        FavoriteClasses a = new FavoriteClasses("Hello", 67, r.get(0));
-//        System.out.println("My favorites are " + a.getFav1() + ", " + a.getFav2() + ", and " + a.getFav3() + ".");
-//    }
+    //Exercise 4 begin
+    private enum ProgrammingLanguage {
+        C("C"), JAVA("Java"), KOTLIN("Kotlin");
+
+        private String languageName;
+
+        ProgrammingLanguage(String languageName) {
+            this.languageName = languageName;
+        }
+
+        @Override
+        public String toString() {
+            return "ProgrammingLanguage{" +
+                    "languageName='" + languageName + '\'' +
+                    '}';
+        }
+    }
+    //Exercise 4 end
+
+    private static class FavoriteClasses<T> {
+        private T favorite1;
+        private T favorite2;
+        private T favorite3;
+
+        FavoriteClasses(T fav1, T fav2, T fav3) {
+            this.favorite1 = fav1;
+            this.favorite2 = fav2;
+            this.favorite3 = fav3;
+        }
+
+        @Override
+        public String toString() {
+            return "FavoriteClasses{" +
+                    "favorite1=" + favorite1 + " " + favorite1.getClass() +
+                    ", favorite2=" + favorite2 + " " + favorite2.getClass() +
+                    ", favorite3=" + favorite3 + " " + favorite3.getClass() +
+                    '}';
+        }
+
+        public T getFav1() {
+            return (this.favorite1);
+        }
+
+        public T getFav2() {
+            return (this.favorite2);
+        }
+
+        public T getFav3() {
+            return (this.favorite3);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //Exercise 2 begin
+        List r = new ArrayList();
+        r.add(6.3);
+        r.add(5.9);
+        FavoriteClasses a = new FavoriteClasses("Hello", 67, r.get(0));
+        FavoriteClasses myFavList = new FavoriteClasses("String", 53f, 292939391);
+        System.out.println("My favorites are " + a.getFav1() + ", " + a.getFav2() + ", and " + a.getFav3() + ".");
+        System.out.println(myFavList);
+        //Exercise 2 end
+
+//        System.out.println(a.favorite1.getClass() + " " + a.favorite2.getClass() + " " + a.favorite3.getClass());
+
+        List myProgrammingLanguages = new ArrayList();
+        myProgrammingLanguages.add(ProgrammingLanguage.C);
+        myProgrammingLanguages.add(ProgrammingLanguage.valueOf("JAVA"));
+
+        System.out.println(myProgrammingLanguages.toString() + " and " + ProgrammingLanguage.KOTLIN.languageName);
+    }
 }
